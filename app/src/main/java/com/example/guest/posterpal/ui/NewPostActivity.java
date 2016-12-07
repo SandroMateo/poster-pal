56d3f014045502cff3d1f6d46b942d585a9984c4
Sandro Alvarez and Satchel Grant <sandro+satchel@>
2016-12-06 16:24:44 -0800

    save posts to firebase when make post button is clicked

--- app/src/main/java/com/example/guest/posterpal/ui/NewPostActivity.java
+++ app/src/main/java/com/example/guest/posterpal/ui/NewPostActivity.java
@@ -3,6 +3,7 @@ package com.example.guest.posterpal.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +12,7 @@ import android.widget.Spinner;
 import com.example.guest.posterpal.Constants;
 import com.example.guest.posterpal.R;
 import com.example.guest.posterpal.models.Category;
+import com.example.guest.posterpal.models.Post;
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,14 +20,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.security.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class NewPostActivity extends AppCompatActivity {
-    private DatabaseReference mCategoryReference;
+    private DatabaseReference ref;
+
 
     @Bind(R.id.usernameEditText) EditText mUserName;
     @Bind(R.id.contentEditText) EditText mContent;
@@ -39,14 +45,15 @@ public class NewPostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_post);
         ButterKnife.bind(this);
 
-        mCategoryReference = FirebaseDatabase.getInstance().getReference().child(Constants.FIREBASE_CHILD_CATEGORIES);
+        ref = FirebaseDatabase.getInstance().getReference();
 
-        mCategoryReference.addValueEventListener(new ValueEventListener() {
+        ref.child(Constants.FIREBASE_CHILD_CATEGORIES).addValueEventListener(new ValueEventListener() {
+
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 final List<String> categories = new ArrayList<String>();
                 for(DataSnapshot categorySnapshot: dataSnapshot.getChildren()) {
-                    String title = categorySnapshot.child("title").getValue().toString();
+                    String title = categorySnapshot.getKey().toString();
                     categories.add(title);
                 }
 
@@ -60,5 +67,22 @@ public class NewPostActivity extends AppCompatActivity {
 
             }
         });
+
+        mSavePostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String category = mCategorySpinner.getSelectedItem().toString();
+                String username = mUserName.getText().toString();
+                String content = mContent.getText().toString();
+                String imageUrl = mImageUrl.getText().toString();
+                String timestamp = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
+                Post newPost = new Post(content, username, timestamp, imageUrl);
+                savePostToFirebase(newPost, category);
+            }
+        });
+    }
+
+    public void savePostToFirebase(Post post, String category) {
+        ref.child(Constants.FIREBASE_CHILD_CATEGORIES).child(category).push().setValue(post);
     }
 }
